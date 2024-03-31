5ea5021f5b6289eb4072bdbe060d86f07742fc50
Dnyaneshwari Sawant <126340898+Dnyanusawant@users.noreply.github.com>
2024-03-31 20:55:18 +0530

    Add files via upload

---
+++ MusicLibrary.java
@@ -0,0 +1,18 @@
+class MusicLibrary
+ {
+    public static void main(String[] args)
+    {
+        short totalSongs = 5000;
+        short rockSongs = 1200; 
+        short popSongs = 1500; 
+        short jazzSongs = 800; 
+        short classicalSongs = 700;
+
+        System.out.println("Total Songs: " + totalSongs);
+        System.out.println("Rock Songs: " + rockSongs);
+        System.out.println("Pop Songs: " + popSongs);
+        System.out.println("Jazz Songs: " + jazzSongs);
+        System.out.println("Classical Songs: " + classicalSongs);
+     
+    }
+}
