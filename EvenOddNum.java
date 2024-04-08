0074f1a4ac06dbaf30f4a7ad90dbd6bc5a01a330
Dnyaneshwari Sawant <126340898+Dnyanusawant@users.noreply.github.com>
2024-04-08 20:37:28 +0530

    Add files via upload

---
+++ EvenOddNum.java
@@ -0,0 +1,15 @@
+class EvenOddNum {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            evenodd(i);
+        }
+    }
+
+    public static void evenodd(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " - even");
+        } else {
+            System.out.println(num + " - odd");
+        }
+    }
+}
---
+++ PrimeNum.java
@@ -0,0 +1,24 @@
+//PrimeNum using method
+
+class PrimeNum {
+    public static void main(String[] args) {
+        for (int i = 2; i < 50; i++) {
+            isPrime(i);
+        }
+    }
+
+    public static void isPrime(int num) {
+        boolean flag = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println(num + " - Prime");
+        } else {
+            System.out.println(num + " - not prime");
+        }
+    }
+}
