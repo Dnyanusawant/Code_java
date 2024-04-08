1188ee9c6abdef04f3e6d219c275dcecfc40cc84
Dnyaneshwari Sawant <126340898+Dnyanusawant@users.noreply.github.com>
2024-04-08 20:38:21 +0530

    Add files via upload

---
+++ DemoExample.java
@@ -0,0 +1,22 @@
+class DemoExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello From DemoExample ");
+        DemoExample.Ramesh();
+        Suresh();
+    }
+
+    public static void Suresh() {
+        System.out.println("Hello from Suresh ");
+    }
+
+    public static void Ramesh() {
+        System.out.println("Hello from Ramesh ");
+    }
+}
+
+class DemoExample1 {
+    public static void main(String[] args) {
+        System.out.println("Hello From DemoExample1 m1()");
+    }
+}
